12155b536d909dbc9689bfb26cb8b3368810f80a
Techett <30886852+techett@users.noreply.github.com>
2017-08-29 21:54:11 -0400

    Update gorpie.py
    Spelling error.. wow lol

--- gorpie.py
+++ gorpie.py
@@ -1,7 +1,7 @@
 '''
 
-gorpie - interest calculator
-by Techett
+Gorpie - A simplistic interest calculator
+By Techett
 
 '''
 
@@ -9,12 +9,12 @@ by Techett
 import time
 
 
-p = input("What is the ammount you would like to calculate for? ")
+p = input("What is the amount you would like to calculate for? ")
 t = input("How long will you be calculating for? ")
 r = input("What is the rate of interest? ")
 r = float(r)/100
 a = int(p)*(1+float(r)*int(t))
-print(a)
+print("Your amount after "+ t + " years wil be: " + a)
 
 
 print()
